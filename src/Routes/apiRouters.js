b337b76db253a0d08d45a9d454a56ae6992d4e19
Yudhriz <riskyyudha678@gmail.com>
2024-12-26 23:31:15 +0700

    fix: fixed routing for GET add SSE

--- src/Routes/apiRouters.js
+++ src/Routes/apiRouters.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const SessionController = require("../Controllers/SessionController");
+const SessionController = require("../Controllers/API/SessionController");
 
 const apiRouters = express.Router();
 
@@ -7,7 +7,7 @@ apiRouters.get("/sessions", SessionController.list);
 apiRouters.get("/sessions/:sessionId", SessionController.find);
 apiRouters.get("/sessions/:sessionId/status", SessionController.status);
 apiRouters.post("/sessions/add", SessionController.add);
-apiRouters.post("/sessions/:sessionId/add-sse", SessionController.addSSE);
+apiRouters.get("/sessions/:sessionId/add-sse", SessionController.addSSE);
 apiRouters.delete("/sessions/:sessionId", SessionController.del);
 
 module.exports = apiRouters;
